78a7b84d6e5580624876966a1b33a0818ace89a2
Marcin Lech <arcinech@gmail.com>
2022-05-11 22:21:40 +0200

    Update: app and index to use new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Header from './components/views/Header/Header';
+import NotFound from './components/pages/NotFound/NotFound';
+import PostEdit from './components/pages/PostEdit/PostEdit';
+import PostAdd from './components/pages/PostAdd/PostAdd';
+import Home from './components/pages/Home/Home';
+import About from './components/pages/About/About';
+import SinglePost from './components/pages/SinglePost/SinglePost';
+import Footer from './components/views/Footer/Footer'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello World!</h2>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:id" element={<SinglePost />} />
+            <Route path="/post/add" element={<PostAdd />} />
+            <Route path="/post/edit/:id" element={<PostEdit />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        <Footer />
+      </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
