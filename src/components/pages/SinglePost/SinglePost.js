bdb40ed62ed41871487a1e6f2aee1b372e53938a
Marcin Lech <arcinech@gmail.com>
2022-05-12 14:00:11 +0200

    Add: content for single posts

--- src/components/pages/SinglePost/SinglePost.js
+++ src/components/pages/SinglePost/SinglePost.js
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getPostById } from "../../../redux/postsRedux";
 
 const SinglePost = () => {
   const {id} = useParams();
+  const post = useSelector(state => getPostById(state, id));
+  console.log(post.id);
   return (
-    <div>Single Post</div>
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.author}</p>
+      <p>{post.publishDate}</p>
+      <div>
+        <p>{post.content}</p>
+      </div>
+    </article>
   );
 
 };
