40b4a68142647b2170060ebf86227d24647fbf98
Marcin Lech <arcinech@gmail.com>
2022-05-12 18:47:45 +0200

    Fix: styles of header and button

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import Button from 'react-bootstrap/button';
 import {Link} from 'react-router-dom';
-import List from '../../features/PostsList/List';
+import List from '../../features/PostList/PostList';
 
 const Home = () => {
   return (
     <>
-      <div>
-        <h2>All Posts</h2>
-        <Button as={Link} to="/post/add" variant="outline-info">Add post</Button>
+      <div className="d-flex justify-content-between">
+        <h2 className="my-auto">All Posts</h2>
+        <div className="my-auto">
+          <Button as={Link} to="/post/add" variant="outline-info">Add post</Button>
+        </div>
       </div>
       <div>
         <List />
