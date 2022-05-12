93eff5ac7c733e2ccb65433e78f83664bd6436c0
Marcin Lech <arcinech@gmail.com>
2022-05-12 13:59:52 +0200

    Add: more styles for List of posts

--- src/components/features/PostsList/List.js
+++ src/components/features/PostsList/List.js
@@ -5,24 +5,22 @@ import Card from "react-bootstrap/Card"
 import {Link} from "react-router-dom";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
+import styles from './List.module.scss';
 
 const List = () => {
   const posts = useSelector(getAllPosts);
   console.log(posts);
 
   return (
-    <Row md={3} sd={2}>
+    <Row md={3} sd={2} className="py-4">
      { posts.map(({id, title, author, publishedDate, shortDescription}) => (
-       <Col className="mb-4">
-        <Card key={id} className="border border-info text-left p-4">
-          <Card.Body>
+       <Col key={id} className="mb-4">
+        <Card className="border border-info text-left p-4 mr-auto">
             <Card.Title>{title}</Card.Title>
             <Card.Text><strong>Author: </strong>{author}</Card.Text>
             <Card.Text><strong>Published: </strong>{publishedDate}</Card.Text>
             <Card.Text>{shortDescription}</Card.Text>
-            <Button as={Link} to={`/post/${id}`}>Read More</Button>
-
-          </Card.Body>
+            <Button as={Link} to={`/post/${id}`} className={`${styles.wMaxContent} mt-auto mb-0 w-auto`}>Read More</Button>
         </Card>
        </Col>
       ))}
