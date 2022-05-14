1dcf088510a90de87aff7fd395dd073ed591ddc2
Marcin Lech <arcinech@gmail.com>
2022-05-14 17:36:20 +0200

    Small change to navbar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -8,10 +8,13 @@ const NavBar = () => {
     <>
       <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 px-4 rounded">
         <Navbar.Brand as={Link} to="/">Blog.app</Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
