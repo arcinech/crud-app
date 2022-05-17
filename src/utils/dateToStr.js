d8f3ca45743c91744168bd446df601626955f81c
Marcin Lech <arcinech@gmail.com>
2022-05-17 22:44:31 +0200

    Add: Fix format of date in dateToStr

--- src/utils/dateToStr.js
+++ src/utils/dateToStr.js
@@ -2,6 +2,6 @@ export const dateToStr = (date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const formatedDate = (day < 10 ? `0${day}` : day) + "/" + (month < 10 ? `0${month}` : month) + `/${year}`;
+  const formatedDate = (month < 10 ? `0${month}` : month) + "/" + (day < 10 ? `0${day}` : day) + `/${year}`;
   return formatedDate;
 };
