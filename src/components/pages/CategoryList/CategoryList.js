9140c21fcae5bbbb51c7f7f2fcc7bead98ae978f
Marcin Lech <arcinech@gmail.com>
2022-05-19 09:07:35 +0200

    Change name of Categories to CategoryList

---
+++ src/components/pages/CategoryList/CategoryList.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { getAllCategories } from "../../../redux/categoriesRedux";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from "react-router-dom";
+import styles from './CategoryList.module.scss'
+
+const CategoryList = () => {
+  const categories = useSelector(getAllCategories);
+  return (
+    <section className={`${styles.contentWidth} mx-auto`}>
+      <h2>Categories</h2>
+      <ListGroup>
+        { categories.map(({id, name}) => (
+          <ListGroup.Item key={id} className="mb-4">
+            <Link to={`/categories/${name}`}>{name}</Link>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </section>
+  );
+};
+
+export default CategoryList;
